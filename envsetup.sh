658b8d63302e6bed6ac5316f48dc69b793944e6f
Ketan Kunde <ketank@us.ibm.com>
2020-08-04 15:30:05 +0530

    added setup file

---
+++ envsetup.sh
@@ -0,0 +1,10 @@
+# script to install tekton pipeline and tkn cli on k8s cluster
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.11.0/tkn_0.11.0_Linux_x86_64.tar.gz
+tar xvzf tkn_0.11.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+
+alias tk-list= 'tkn task list -n tekton-pipeline' # List all task in namespace specified
+alias tkr-list= 'tkn taskrun list -n tekton-pipeline' # List all task runs in namespace specified
+alias pip-list= 'tkn pipeline list -n tekton-pipeline' # List all pipeline in namespace specified
+alias pipr-list= 'tkn pipelinerun list -n tekton-pipeline' # List all pipeline runs in namespace specified

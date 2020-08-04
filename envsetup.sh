6b977c71edeab8a644a391a63efd8d00cfb1a58c
Ketan Kunde <ketank@us.ibm.com>
2020-08-04 16:07:32 +0530

    changes done

--- envsetup.sh
+++ envsetup.sh
@@ -4,7 +4,7 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline
 curl -LO https://github.com/tektoncd/cli/releases/download/v0.11.0/tkn_0.11.0_Linux_x86_64.tar.gz
 tar xvzf tkn_0.11.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
 
-alias tk-list= 'tkn task list -n tekton-pipeline' # List all task in namespace specified
-alias tkr-list= 'tkn taskrun list -n tekton-pipeline' # List all task runs in namespace specified
-alias pip-list= 'tkn pipeline list -n tekton-pipeline' # List all pipeline in namespace specified
-alias pipr-list= 'tkn pipelinerun list -n tekton-pipeline' # List all pipeline runs in namespace specified
+alias tk-list= 'tkn task list -n tekton-pipelines' # List all task in namespace specified
+alias tkr-list= 'tkn taskrun list -n tekton-pipelines' # List all task runs in namespace specified
+alias pip-list= 'tkn pipeline list -n tekton-pipelines' # List all pipeline in namespace specified
+alias pipr-list= 'tkn pipelinerun list -n tekton-pipelines' # List all pipeline runs in namespace specified

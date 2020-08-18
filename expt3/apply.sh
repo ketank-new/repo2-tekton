1f5119a8c7b0ef8a1516623d38bb8dec9328060a
ketank-new <ketan22584@gmail.com>
2020-08-18 13:38:10 +0530

    Create apply.sh

---
+++ expt3/apply.sh
@@ -0,0 +1,9 @@
+kubectl apply -f secret-git.yaml
+kubectl apply -f serviceAccount.yaml
+kubectl apply -f resource.yaml
+kubectl apply -f ../expt2-docker-pull/newsecret.yaml
+kubectl apply -f Task.yaml
+kubectl apply -f run.yaml
+
+alias chk='tkn taskrun ls'
+alias pods='kubectl get pods'

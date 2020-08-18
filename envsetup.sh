01a53ccef8bd98ef0ec6b7cb8005cec4eb8ce293
ketank-new <ketan22584@gmail.com>
2020-08-18 11:06:01 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -3,6 +3,7 @@
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 curl -LO https://github.com/tektoncd/cli/releases/download/v0.11.0/tkn_0.11.0_Linux_x86_64.tar.gz
 tar xvzf tkn_0.11.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+kubectl config set-context --current --namespace=tekton-pipelines
 
 alias tk-list= 'tkn task list -n tekton-pipelines' # List all task in namespace specified
 alias tkr-list= 'tkn taskrun list -n tekton-pipelines' # List all task runs in namespace specified

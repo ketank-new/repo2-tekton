f362a1da13735c90146a15624a08aa88c6b788c9
ketank-new <ketan22584@gmail.com>
2020-08-20 09:54:59 +0530

    Update apply.sh

--- expt3/apply.sh
+++ expt3/apply.sh
@@ -2,7 +2,8 @@ kubectl apply -f secret-git.yaml
 kubectl apply -f serviceAccount.yaml
 kubectl apply -f resource.yaml
 kubectl apply -f resourcetwo.yaml
-kubectl apply -f ../expt2-docker-pull/newsecret.yaml
+kubectl apply -f dockersec.yaml
+#kubectl apply -f ../expt2-docker-pull/newsecret.yaml
 kubectl apply -f Task.yaml
 kubectl apply -f run.yaml
 
